c232e5c168f95a150c7a4a5f4c404e5822e54eed
Hengker EpEp <167304514+THEOYS123@users.noreply.github.com>
2024-05-12 01:26:37 +0700

    Update fb123.sh

--- fb123.sh
+++ fb123.sh
@@ -6,77 +6,52 @@ GREEN-Ve[92m"
 
 RED-"\e[31m"
 
-clear
-
-sleep 1
-
-echo -e
-
-echo -e
-
-CRAK FB PREMIUM"
-
-echo -e
-
-echo -e Author: MR.HOOH
-
-echo -e YOUTUBE: MR.HOOH"
-
-echo -e
-
-echo -e 1.Crack"
-
-echo-e 2. Update SC
-
+echo -e CRACK FB PREMIUM"
+echo -e 1 Crack"
+echo -e 2 Update SC"
 echo -e 3.exit"
-
 echo
-
-read -p "PILIH NO HOOH
-
-if [ SHOOH 1]
-
+read -p PILIH NO" HOOH
+if [ SHOOH == 1]"
 then
-
-+82%
 sleep 50
 echo
-echo -e $(GREEN)devilkage4@gmail.com | Devil_999
+echo-e " $(GREEN)devilkage4@gmail.com | Devil_999"
 sleep 400
 echo
-echo -e $(RED)danipratama01@gmail.com | Den-p_0923
-sleep 500 
+echo -e "$(RED)danipratama01@gmail.com | Den-p_0923"
+sleep 500
 echo
-echo -e $(BLUE) nugrohosamson21@gmail.com | Samson 111
+echo -e "$(GREEN) narutofootbal@gmail.com | Naruto_123"
 sleep 90
 echo
-echo -e $(GREEN) narutofootbal@gmail.com | Naruto_123
+echo -e "$(RED)irpan020465@gmail.com | irpan@01928"
 sleep 300
 echo
-echo -e $(RED)ma110183820@gmail.com | 0273@#&%9_maill
-$(GREEN) nugrohosamson21@gmail.com | Samson 111
-echo -e $(RED)irpan020465@gmail.com | irpan@01928
-sleep 300
-echo
-echo -e
+echo -e "$(RED)ma110183820@gmail.com |1221234@"
 sleep 200
 echo
-echo
-echo-e
+echo -e "$(GREEN)nugrohosamson21@gmail.com | Samson_111"
 sleep 29
 echo
-echo -e $(GREEN)DONE
+echo -e "$(GREEN)DONE"
 sleep 1
 cd Shome
 rm -rf fb123
 fi
 
-if [ SHOOH2]
+if [ SHOOH == 2 ]
 then
 sleep 1
 clear
-xdg-open "https://wa.me/+6289519450908text.bree gua mau beli tools n
+xdg-open"https://wa.me/+6289519450908text.bree gua mau beli tools"
 clear
 sleep 10
 echo -e TERIMA KASIH"
+fi
+
+if [ SHOOH == 3 ]
+then
+sleep 2
+echo -e " kenapa gak keluar?"
 fi
