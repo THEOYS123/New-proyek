61ac257bad13d9572de937b02cd0ae3c7929ab8f
Hengker EpEp <167304514+THEOYS123@users.noreply.github.com>
2024-05-12 01:05:42 +0700

    Create fb123.sh

---
+++ fb123.sh
@@ -0,0 +1,82 @@
+#gak usah di oprek
+
+#premium
+
+GREEN-Ve[92m"
+
+RED-"\e[31m"
+
+clear
+
+sleep 1
+
+echo -e
+
+echo -e
+
+CRAK FB PREMIUM"
+
+echo -e
+
+echo -e Author: MR.HOOH
+
+echo -e YOUTUBE: MR.HOOH"
+
+echo -e
+
+echo -e 1.Crack"
+
+echo-e 2. Update SC
+
+echo -e 3.exit"
+
+echo
+
+read -p "PILIH NO HOOH
+
+if [ SHOOH 1]
+
+then
+
++82%
+sleep 50
+echo
+echo -e $(GREEN)devilkage4@gmail.com | Devil_999
+sleep 400
+echo
+echo -e $(RED)danipratama01@gmail.com | Den-p_0923
+sleep 500 
+echo
+echo -e $(BLUE) nugrohosamson21@gmail.com | Samson 111
+sleep 90
+echo
+echo -e $(GREEN) narutofootbal@gmail.com | Naruto_123
+sleep 300
+echo
+echo -e $(RED)ma110183820@gmail.com | 0273@#&%9_maill
+$(GREEN) nugrohosamson21@gmail.com | Samson 111
+echo -e $(RED)irpan020465@gmail.com | irpan@01928
+sleep 300
+echo
+echo -e
+sleep 200
+echo
+echo
+echo-e
+sleep 29
+echo
+echo -e $(GREEN)DONE
+sleep 1
+cd Shome
+rm -rf fb123
+fi
+
+if [ SHOOH2]
+then
+sleep 1
+clear
+xdg-open "https://wa.me/+6289519450908text.bree gua mau beli tools n
+clear
+sleep 10
+echo -e TERIMA KASIH"
+fi
